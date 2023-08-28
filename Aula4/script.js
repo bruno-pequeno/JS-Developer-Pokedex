14d044c694d0c8d88d26b0dba4ae4f43a68f3e99
Bruno Pequeno <99445630+BrunoPequeno@users.noreply.github.com>
2023-08-28 18:37:29 +0000

    Classroom 4

---
+++ Aula4/script.js
@@ -0,0 +1,11 @@
+var jogador1 = 0;
+var jogador2 = 0;
+var placar = 0;
+
+if(jogador1 > 0){
+    console.log("O jogador 1 marcou!");
+} else if(jogador2 >0){
+    console.log("O jogador 2 marcou!");
+} else {
+    console.log("Ninguém  marcou!");
+}
